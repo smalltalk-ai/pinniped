2572b42a1031c8d7a941decf048d2f0df669b77d
michow <michow@smalltalk.agency>
2021-02-26 15:08:53 -0800

    applicable updates based on feedback

--- site/themes/pinniped/static/js/main.js
+++ site/themes/pinniped/static/js/main.js
@@ -1,7 +1,7 @@
 "use strict";
 
 function mobileNavToggle() {
-    var menu = document.getElementById("mobile-menu").parentElement;
+    var menu = document.getElementById('mobile-menu').parentElement;
     menu.classList.toggle('mobile-menu-visible');
 }
 document.addEventListener('DOMContentLoaded', function () {
